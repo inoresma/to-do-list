a4d10f7cb8e78c92ff5653574316b780ec88984e
inoresma <inoresma@gmail.com>
2024-12-12 17:02:51 -0300

    Inicializacion TailwindCSS

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,31 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
+<script setup>
 import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
+<template>
+  <div class="min-h-screen bg-blue-900 py-6 flex flex-col justify-center sm:py-12">
+    <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+      <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+        <div class="max-w-md mx-auto">
+          <div class="divide-y divide-gray-200">
+            <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+              <div class="flex items-center space-x-4">
+                <a href="https://vitejs.dev" target="_blank">
+                  <img src="/vite.svg" class="h-12 w-12 hover:scale-110 transition-transform" alt="Vite logo" />
+                </a>
+                <a href="https://vuejs.org/" target="_blank">
+                  <img src="./assets/vue.svg" class="h-12 w-12 hover:scale-110 transition-transform" alt="Vue logo" />
+                </a>
+              </div>
+              <HelloWorld msg="Vite + Vue + Tailwind" class="mt-8" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* You can remove the previous styles since we'll use Tailwind classes */
 </style>
